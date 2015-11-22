a79b5c9a78a341e37aa2286c89e2f98a0a8736c7
jukyokim <jukyokim1992@gmail.com>
2015-11-22 18:18:12 -0500

    was able to remove kinks need to write a unique ID finder and a remover

--- helper_functions/twit_helper.js
+++ helper_functions/twit_helper.js
@@ -9,24 +9,80 @@ twitter = new Twit({
 });
 
 module.exports = {
-	// returns the a json object of all the tweets based on that search
-	searchByTerm:function(term, callback){
-		//create todays date
-		var dateObject = new Date(),
-		todaysDate = dateObject.getFullYear() + "-" + dateObject.getMonth() + "-" +dateObject.getDate();
+  // returns the a json object of all the tweets based on that search
+  searchByTerm:function(term, callback){
+    //create todays date
+    var dateObject = new Date(),
+    todaysDate = dateObject.getFullYear() + "-" + dateObject.getMonth() + "-" +dateObject.getDate();
 
-		//makes an initial search of the term
-		twitter.get('search/tweets', { q: term + ' since:' + todaysDate, count: 49 }, function(err, data, response) {
-  			if(err){
-  				callback.send(err);// error handling
-  			}else if(data.statuses.length === 0){
-  				callback.sendstatus(404);//check to see if anyone used this work ever
-  			}else{
-  				var maxId = data.statuses[data.statuses.length-1].id; // sets the lowest id
-  				twitter.get('search/tweets', { q: term + ' since:' + todaysDate+'&max_id=' + maxId, count: 100 }, function(err, data, response) {
-  					err ? callback.send(err) : callback.send(data); //response is called back and sends data back or sends error
-				});// end of the secondary twitter data grab
-  			}// end of the if else conditional for error check / nothing found check
-		});// end of the initial twitter data grab
-	}// end of the searchbyterm function
+    //makes an initial search of the term
+    twitter.get('search/tweets', { q: term, since: todaysDate, count: 100 }, function(err, data, response) {
+        if(err){
+          callback.send(err);// error handling
+        }else if(data.statuses.length === 0){
+          callback.send({
+            "response": false
+          });//check to see if anyone used this work ever
+        }else{
+          
+          var stat = data.statuses;
+          var newObject = {
+            "response": true,
+            "tweetCount": 0,
+            "max_id": 0,
+            "tweets":[]            
+          };
+  
+          for(var i = 0; i < stat.length;i++){
+            var formattedData = {
+              "tweet_id": stat[i].id,
+              "count": i,
+              "username": stat[i].user.name,
+              "screen_name": stat[i].user.screen_name,
+              "text": stat[i].text,
+              "created_at": stat[i].created_at,
+              "total_likes_user_has": stat[i].user.favourites_count,
+              "followers": stat[i].user.followers_count,
+              "followings": stat[i].user.friends_count,
+              "profile_img": stat[i].user.profile_image_url
+            };
+            console.log("item added in first dataset");
+            newObject.tweets.push(formattedData);
+          }//end of for loop
+
+          var maxId = data.search_metadata.max_id;
+
+          twitter.get('search/tweets', { q: term, since_id: maxId, count: 100 }, function(err, data, response){
+            var stat = data.statuses;
+
+            for(var e = 0; e < stat.length;e++){
+              var formattedData = {
+                "tweet_id": stat[e].id,
+                "count": e+100,
+                "username": stat[e].user.name,
+                "screen_name": stat[e].user.screen_name,
+                "text": stat[e].text,
+                "created_at": stat[e].created_at,
+                "total_likes_user_has": stat[e].user.favourites_count,
+                "followers": stat[e].user.followers_count,
+                "followings": stat[e].user.friends_count,
+                "profile_img": stat[e].user.profile_image_url
+              };
+              console.log("item added in second dataset");
+              newObject.tweets.push(formattedData);
+            }//end of for loop
+            console.log(data.search_metadata.max_id + "it got here");
+            callback.send(newObject);
+
+          });
+
+
+
+
+
+
+
+        }// end of the if else conditional for error check / nothing found check
+    });// end of the initial twitter data grab
+  }// end of the searchbyterm function
 };
