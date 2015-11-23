fb5199a0593c59541055f635ce7bb4ed3fd1500c
jukyokim <jukyokim1992@gmail.com>
2015-11-22 19:59:48 -0500

    tweets per second

--- helper_functions/twit_helper.js
+++ helper_functions/twit_helper.js
@@ -30,13 +30,14 @@ module.exports = {
             "response": true,
             "tweetCount": 0,
             "max_id": 0,
-            "tweets":[]            
+            "tweetsPerSec":0,
+            "tweets":[],
+            "completed_times":[]            
           };
   
           for(var i = 0; i < stat.length;i++){
             var formattedData = {
               "tweet_id": stat[i].id,
-              "count": i,
               "username": stat[i].user.name,
               "screen_name": stat[i].user.screen_name,
               "text": stat[i].text,
@@ -46,19 +47,23 @@ module.exports = {
               "followings": stat[i].user.friends_count,
               "profile_img": stat[i].user.profile_image_url
             };
-            console.log("item added in first dataset");
             newObject.tweets.push(formattedData);
           }//end of for loop
 
+// NOTE!!! below this line is a nesting hell
+// NOTE!!! I need to find a way to recursivly do the bottom part.
+// NOTE!!! I tried to do it but its not working and stops after the first try
+
+
+          newObject.completed_times.push(data.search_metadata.completed_in);
           var maxId = data.search_metadata.max_id;
 
           twitter.get('search/tweets', { q: term, since_id: maxId, count: 100 }, function(err, data, response){
             var stat = data.statuses;
 
-            for(var e = 0; e < stat.length;e++){
+            for(var e = 1; e < stat.length;e++){
               var formattedData = {
                 "tweet_id": stat[e].id,
-                "count": e+100,
                 "username": stat[e].user.name,
                 "screen_name": stat[e].user.screen_name,
                 "text": stat[e].text,
@@ -68,17 +73,106 @@ module.exports = {
                 "followings": stat[e].user.friends_count,
                 "profile_img": stat[e].user.profile_image_url
               };
-              console.log("item added in second dataset");
-              newObject.tweets.push(formattedData);
+              newObject.tweets.push(formattedData);            
             }//end of for loop
-            console.log(data.search_metadata.max_id + "it got here");
-            callback.send(newObject);
+            newObject.completed_times.push(data.search_metadata.completed_in);
+            max_id = data.search_metadata.max_id;
+            twitter.get('search/tweets', { q: term, since_id: maxId, count: 100 }, function(err, data, response){
+              var stat = data.statuses;
 
-          });
+              for(var e = 1; e < stat.length;e++){
+                var formattedData = {
+                  "tweet_id": stat[e].id,
+                  "username": stat[e].user.name,
+                  "screen_name": stat[e].user.screen_name,
+                  "text": stat[e].text,
+                  "created_at": stat[e].created_at,
+                  "total_likes_user_has": stat[e].user.favourites_count,
+                  "followers": stat[e].user.followers_count,
+                  "followings": stat[e].user.friends_count,
+                  "profile_img": stat[e].user.profile_image_url
+                };
+                newObject.tweets.push(formattedData);
+                
+              }//end of for loop
+              newObject.completed_times.push(data.search_metadata.completed_in);
+              
+              max_id = data.search_metadata.max_id;
+              twitter.get('search/tweets', { q: term, since_id: maxId, count: 100 }, function(err, data, response){
+                var stat = data.statuses;
+                for(var e = 1; e < stat.length;e++){
+                  var formattedData = {
+                    "tweet_id": stat[e].id,
+                    "username": stat[e].user.name,
+                    "screen_name": stat[e].user.screen_name,
+                    "text": stat[e].text,
+                    "created_at": stat[e].created_at,
+                    "total_likes_user_has": stat[e].user.favourites_count,
+                    "followers": stat[e].user.followers_count,
+                    "followings": stat[e].user.friends_count,
+                    "profile_img": stat[e].user.profile_image_url
+                  };
+                  newObject.tweets.push(formattedData);
+                  
+                }//end of for loop
+                newObject.completed_times.push(data.search_metadata.completed_in);
 
+                max_id = data.search_metadata.max_id;
+                twitter.get('search/tweets', { q: term, since_id: maxId, count: 100 }, function(err, data, response){
+                  var stat = data.statuses;
+                  for(var e = 1; e < stat.length;e++){
+                    var formattedData = {
+                      "tweet_id": stat[e].id,
+                      "username": stat[e].user.name,
+                      "screen_name": stat[e].user.screen_name,
+                      "text": stat[e].text,
+                      "created_at": stat[e].created_at,
+                      "total_likes_user_has": stat[e].user.favourites_count,
+                      "followers": stat[e].user.followers_count,
+                      "followings": stat[e].user.friends_count,
+                      "profile_img": stat[e].user.profile_image_url
+                    };
+                    newObject.tweets.push(formattedData);
+                    
+                  }//end of for loop
+                  newObject.completed_times.push(data.search_metadata.completed_in);
+                  
+                  max_id = data.search_metadata.max_id;
+                  twitter.get('search/tweets', { q: term, since_id: maxId, count: 100 }, function(err, data, response){
+                    var stat = data.statuses;
+                    for(var e = 1; e < stat.length;e++){
+                      var formattedData = {
+                        "tweet_id": stat[e].id,
+                        "username": stat[e].user.name,
+                        "screen_name": stat[e].user.screen_name,
+                        "text": stat[e].text,
+                        "created_at": stat[e].created_at,
+                        "total_likes_user_has": stat[e].user.favourites_count,
+                        "followers": stat[e].user.followers_count,
+                        "followings": stat[e].user.friends_count,
+                        "profile_img": stat[e].user.profile_image_url
+                      };
+                      newObject.tweets.push(formattedData);
+                      
+                    }//end of for loop
+                    newObject.completed_times.push(data.search_metadata.completed_in);
+                    newObject.tweetCount = newObject.tweets.length;
 
+                    var sumTime = newObject.completed_times.reduce(function(num1, num2){ return num1 + num2; },0);
+                    console.log(sumTime);
+                    var tweetsPerSecCalc = (100/(Math.round(sumTime * 100))* newObject.tweetCount);
 
+                    newObject.tweetsPerSec = tweetsPerSecCalc;
+                    callback.send(newObject);
+                  });// sixth twitter grab
+                  
+                });// fifth twitter grab
 
+              });// fourth twitter grab
+
+            });// third twitter grab
+
+          });// second twitter grab
 
         }// end of the if else conditional for error check / nothing found check
     });// end of the initial twitter data grab
