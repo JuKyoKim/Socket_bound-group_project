fcfc95d2c15a7f0da1f6b3163b0a9c5dd7e8fd5b
jukyokim <jukyokim1992@gmail.com>
2015-11-24 16:26:02 -0500

    background

--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-console.log('app is loaded');
-
-$(document).ready(function(){
-	console.log("jquery is in yall");
-});
--- public/js/game_engine/main.js
+++ public/js/game_engine/main.js
@@ -3,92 +3,29 @@ $(document).ready(function(){
 	game;
 });
 
+var game = new Phaser.Game(700, 500, Phaser.AUTO, 'game_container', { preload: preload, create: create, update: update });
 
-
-var game = new Phaser.Game(700, 500, Phaser.AUTO, 'game_container', { preload: preload, create: create, update: update, render: render });
-var floor;
-var player;
-var player1;
-var player2;
-
-function preload() {
+function preload(){
+	//preloads the Physics in to the game
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.load.image('background','../../assets/background.png');
-	game.load.image('floor','../../assets/placeholdfloor.png');
+
+	//preloads all the backgrounds
+	game.load.image('background1','../../assets/background1.png');
+	game.load.image('background2','../../assets/background2.png');
+	game.load.image('background3','../../assets/background3.png');
+	game.load.image('background4','../../assets/background4.png');
+
 	game.load.image('player1','../../assets/player1.png');
 	game.load.image('player2','../../assets/player2.png');
 }
 
-function create() {
-	//background image
-	game.add.sprite(0, 0, 'background');
-	
-	//grouping that joint, in case i need to display multiple
-	floor = game.add.group();
-	//enables physics for the floor
-	floor.enableBody = true;
-	//creates the ground
-	var ground = floor.create(0, game.world.height - 100, 'floor');
-	
-	//scales it to fit the page
-	ground.scale.setTo(4, 1);
-	
-	//makes the ground static
-	ground.body.immovable = true;
-	
-	//new player group(mainly to attach properties)
-	player = game.add.group();
-	
-	//player 1 and 2
-	player1 = player.create(0, game.world.height - 300, 'player1');
-	player2 = player.create(100, game.world.height - 300, 'player2');
-	
-	//enabling arcade physics for the player
-	game.physics.arcade.enable(player);
-	
-	//using the hlper function to set the properties
-	setPlayerProperties(player1);
-	setPlayerProperties(player2);
-
-
+function create(){
+	var rand = Math.round(Math.random()*3)+1
+	//the background will tile if the background is smaller than the preset size i set above
+	game.add.tileSprite(0,0, game.world.width, game.world.height, 'background'+rand);
 
 }
 
-function update() {
-	//collision detection agaisnt the floor and the players
-	game.physics.arcade.collide(floor, player);
-	
-	//creates keyboard keys
-	directKeys = game.input.keyboard.createCursorKeys();
-	setkeys(player1);
-	setkeys(player2);
-
+function update(){
 
 }
-
-function render() {
-
-}
-
-//helper functions for the game
-//helper functions for the game
-
-var setPlayerProperties = function(player){
-	player.body.bounce.y = 0.2;
-	player.body.gravity.y = 300;
-	player.body.collideWorldBounds = true;
-}
-
-var setkeys = function(player){
-	player.body.velocity.x = 0;
-
-	if(directKeys.left.isDown){
-		//go left
-		player.body.velocity.x = -150;
-	}else if(directKeys.right.isDown){
-		//go right
-		player.body.velocity.x = 150;
-	}
-}
-
-
